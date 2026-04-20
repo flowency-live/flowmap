1cb21b4f217253c7872aeb2495a95b9798d5de15
BandFlow25 <jason@flowency.co.uk>
2026-04-20 14:56:11 +0100

    fix: remove OPTIONS from Lambda URL CORS (auto-handled)

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -37,7 +37,7 @@ const functionUrl = validateTokenLambda.addFunctionUrl({
   cors: {
     allowedOrigins: ['*'],
     allowedHeaders: ['content-type'],
-    allowedMethods: [HttpMethod.POST, HttpMethod.OPTIONS],
+    allowedMethods: [HttpMethod.POST],
   },
 });
 
