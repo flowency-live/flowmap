52196bc6506c366307bbc574328cb68bce73b948
BandFlow25 <jason@flowency.co.uk>
2026-04-21 10:29:15 +0100

    fix: assign invitation-api to auth stack to avoid circular dependency

--- amplify/functions/invitation-api/resource.ts
+++ amplify/functions/invitation-api/resource.ts
@@ -6,4 +6,5 @@ export const invitationApi = defineFunction({
   runtime: 20,
   timeoutSeconds: 30,
   memoryMB: 256,
+  resourceGroupName: 'auth', // Assign to auth stack since it depends on User Pool
 });
